e5ac1cfc971605d3d8bbd63c9345c12ca4cb8d07
MacBook Pro <quoccuong.ukh97@gmail.com>
2023-03-09 17:51:45 +0700

    server cor

--- server.js
+++ server.js
@@ -19,9 +19,10 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true'); // Add this line
   next();
 });
 // {
