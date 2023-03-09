fd610d11fcd2ad930e711abaa8726991c08a281b
MacBook Pro <quoccuong.ukh97@gmail.com>
2023-03-09 17:55:26 +0700

    es

--- server.js
+++ server.js
@@ -18,19 +18,20 @@ app.use(cors({
   credentials: true
 }));
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); // Add this line
-  next();
-});
 // {
 //   origin: 'http://localhost:3000',
 //   credentials: true
 // }
 app.use(cookieParser())
 app.use(passport.initialize())
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 //import 
 
