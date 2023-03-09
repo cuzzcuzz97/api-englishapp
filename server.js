334d48fdde2d0e35589b4c5cc12f9aff561b135b
MacBook Pro <quoccuong.ukh97@gmail.com>
2023-03-09 15:07:53 +0700

    server port

--- server.js
+++ server.js
@@ -29,10 +29,10 @@ app.use('/words', inputRouter);
 app.use('/', authRouter);
 
 
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
 }).on('error', (err) => {
   console.error(`Server error: ${err.message}`);
 });
