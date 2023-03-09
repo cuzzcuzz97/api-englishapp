72fc0af4593dbe385d81f8e4278de99af83a90f6
MacBook Pro <quoccuong.ukh97@gmail.com>
2023-03-09 14:58:56 +0700

    change server cors

--- server.js
+++ server.js
@@ -13,10 +13,11 @@ const passport = require('passport')
 require('./middlewares/passport-middleware')
 
 app.use(bodyParser.json());
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}));
+app.use(cors());
+// {
+//   origin: 'http://localhost:3000',
+//   credentials: true
+// }
 app.use(cookieParser())
 app.use(passport.initialize())
 
