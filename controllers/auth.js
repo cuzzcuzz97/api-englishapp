5c707018e0c684b63a0027c937fd6a5e8aea80a0
MacBook Pro <quoccuong.ukh97@gmail.com>
2023-03-09 16:09:07 +0700

    add console tokens cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,6 +20,7 @@ exports.getUsers = async (req,res) => {
 exports.getUser = async (req, res) => {
     try {
     console.log(req.cookies.token)
+    console.log(req.cookie.token)
       const decoded = verify(req.cookies.token, SECRETKEY);
       const user_id = decoded.id;
   
