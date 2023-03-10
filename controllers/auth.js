0ad49ff9c9566134cb97262b07e96b58644355e8
MacBook Pro <quoccuong.ukh97@gmail.com>
2023-03-10 15:29:43 +0700

    auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -67,9 +67,8 @@ exports.login = async (req,res) => {
         email: user.email,
     }
     try {
-        const token = await sign(payload, SECRETKEY);
-        console.log(token)
-        res.cookie('token',token, { httpOnly: true, secure: false })
+        const token = await sign(payload, SECRETKEY, { expiresIn: '1h' });
+        res.cookie('token', token, { httpOnly: true, secure: false, maxAge: 3600000 });
         return res.status(200).json({
             success: true,
             message: 'Logged in successfully',
